f6709a063bdff0f5eaa98db423cdad28747e2bf0
Sojin V Saji <sojinvsaji777@gmail.com>
2024-06-23 15:35:46 +0530

    app bar updated

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -23,80 +23,95 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: MyDrawer(),
-      appBar: AppBar(
-        toolbarHeight: 70,
-        backgroundColor: Pallete.tertiaryColors,
-        title: const Text(
-          "scribe",
-          style: TextStyle(fontSize: 25),
-        ),
-        centerTitle: true,
-      ),
-      floatingActionButton: MyFloatingButton(),
-      body: RefreshIndicator(
-        backgroundColor: Pallete.tertiaryColors,
-        color: Pallete.secondaryColor,
-        onRefresh: _pullToRefresh,
-        child: AnimationLimiter(
-          child: MasonryGridView.builder(
-              itemCount: 8,
-              gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2),
-              itemBuilder: (builder, ctx) {
-                return AnimationConfiguration.staggeredGrid(
-                  position: ctx,
-                  duration: Duration(milliseconds: 500),
-                  columnCount: 2,
-                  child: ScaleAnimation(
-                    child: FadeInAnimation(
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            top: 8, left: 6, right: 6, bottom: 6),
-                        child: GestureDetector(
-                          onTap: () {
-                            HapticFeedback.mediumImpact();
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => EditNotePage()));
-                          },
-                          onLongPress: () {
-                            HapticFeedback.vibrate();
-                            _showMyDialog(context);
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Pallete.tertiaryColors,
-                              border: Border.all(color: Pallete.borderColor),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    'Note title',
-                                    style: TextStyle(fontSize: 20),
-                                  ),
+    return SafeArea(
+      child: Scaffold(
+        drawer: MyDrawer(),
+        // appBar: AppBar(
+        //   toolbarHeight: 70,
+        //   backgroundColor: Pallete.tertiaryColors,
+        //   title: const Text(
+        //     "scribe",
+        //     style: TextStyle(fontSize: 25),
+        //   ),
+        //   centerTitle: true,
+        // ),
+        floatingActionButton: MyFloatingButton(),
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+            SliverAppBar(
+              backgroundColor: Pallete.tertiaryColors,
+              floating: true,
+              snap: true,
+              title: Text("scribe", style: TextStyle(fontSize: 25),),
+              centerTitle: true,
+              toolbarHeight: 70,
+            ),
+          ],
+          
+          body: RefreshIndicator(
+            backgroundColor: Pallete.tertiaryColors,
+            color: Pallete.secondaryColor,
+            onRefresh: _pullToRefresh,
+            child: AnimationLimiter(
+              child: MasonryGridView.builder(
+                  itemCount: 8,
+                  gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (builder, ctx) {
+                    return AnimationConfiguration.staggeredGrid(
+                      position: ctx,
+                      duration: Duration(milliseconds: 500),
+                      columnCount: 2,
+                      child: ScaleAnimation(
+                        child: FadeInAnimation(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top : 8,left: 6, right: 6, bottom: 6),
+                            child: GestureDetector(
+                              onTap: () {
+                                HapticFeedback.mediumImpact();
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => EditNotePage()));
+                              },
+                              onLongPress: () {
+                                HapticFeedback.vibrate();
+                                _showMyDialog(context);
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Pallete.tertiaryColors,
+                                  border: Border.all(color: Pallete.borderColor),
+                                  borderRadius: BorderRadius.circular(10),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "dfjhkdaf asdfjaksldf jaksldjflkas dflkasjdflkasj dfklasdjfkl asdfjkasd flkasdfjaksd fjaksdfkalsdf klasdjflkasdflkasdfjaskdf jlaksdjf sdkjfhskd sdfsakldfjasd fasdfjaskdf sdklkkkdkdk kljdfhkjlsdhfjkasdhflkjasdhfkjashdfkjashdfkjashdfkjashdfkjashdfkjashdfkjashdfkjashdfkjashdfkjasdhfkljashdfkjasdhf asdfhalskd faksdf hamsdjhfkja sdfhkasj dfhkasjdfh kasjdfhkjasd fkasjldfhlwf aksjldfh vkjs hskdjfh kjasdfhjkasd sdkj",
-                                    maxLines: 20,
-                                  ),
-                                )
-                              ],
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Note title',
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "dfjhkdaf asdfjaksldf jaksldjflkas dflkasjdflkasj dfklasdjfkl asdfjkasd flkasdfjaksd fjaksdfkalsdf klasdjflkasdflkasdfjaskdf jlaksdjf sdkjfhskd sdfsakldfjasd fasdfjaskdf sdklkkkdkdk kljdfhkjlsdhfjkasdhflkjasdhfkjashdfkjashdfkjashdfkjashdfkjashdfkjashdfkjashdfkjashdfkjashdfkjasdhfkljashdfkjasdhf asdfhalskd faksdf hamsdjhfkja sdfhkasj dfhkasjdfh kasjdfhkjasd fkasjldfhlwf aksjldfh vkjs hskdjfh kjasdfhjkasd sdkj",
+                                        maxLines: 20,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                );
-              }),
+                    );
+                  }),
+            ),
+          ),
         ),
       ),
     );
